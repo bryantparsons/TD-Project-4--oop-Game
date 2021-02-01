3a9646b09b934c61a872405238673a9e06c5e7f1
bryantparsons <58860704+bryantparsons@users.noreply.github.com>
2021-02-01 18:21:18 -0500

    Added rndom phrase generator

---
+++ js/Game.js
@@ -0,0 +1,37 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+ class Game {
+     constructor(){
+         this.missed = 0;
+         this.phrases = this.createPhrases();
+         this.activePhrases = 'null';
+     }
+
+/**
+* Creates phrases for use in game
+* @return {array} An array of phrases that could be used in the game
+*/
+createPhrases() {
+    const phrases = [
+        new Phrase ('Close But No Cigar'),
+        new Phrase ('A Fool and His Money Are Soon Parted'),
+        new Phrase ('Between a Rock and a Hard Place'),
+        new Phrase ('I hate to eat and run'),
+        new Phrase ('off the top of my head')
+    ];
+
+    return phrases;
+}
+
+/**
+* Selects random phrase from phrases property
+* @return {Object} Phrase object chosen to be used
+*/
+getRandomPhrase() {
+   const PhraseNum = Math.floor(Math.random() * this.phrases.length);
+  return this.phrases[PhraseNum];
+}
+
+}
